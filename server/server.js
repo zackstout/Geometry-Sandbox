9b44931cb7d36246101e145bbb6a9f2ed9e5fd57
Zack Stout <zackstout@gmail.com>
2017-11-10 14:55:54 -0600

    up and running with viewsgit add .git add .

---
+++ server/server.js
@@ -0,0 +1,11 @@
+
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 6660;
+
+app.use(express.static('server/public'));
+
+app.listen(port, function (req, res) {
+  console.log('Listening on port', port);
+});
