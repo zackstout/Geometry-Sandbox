0494b90ec88d59c9f26a968eafe99ce32c74f059
Zack Stout <zackstout@gmail.com>
2017-11-11 13:32:22 -0600

    ok it finally seems to know when it's hitting the parabola, time to draw a tangent

--- server/public/controllers/parab_shooter_controller.js
+++ server/public/controllers/parab_shooter_controller.js
@@ -5,37 +5,53 @@ app.controller('ParabShooterController', function () {
       var ctx = canvas.getContext("2d");
     var vm = this;
   vm.params = {};
+  var iteration = 0;
+
 
   parabola(70, 5, 1, 0, 1, 1);
   ctx.transform(-1, 0, 0, 1, 0, 0);
   parabola(70, 5, 1, 0, 1, -1);
   ctx.transform(-1, 0, 0, 1, 0, 0);
   ctx.beginPath();
-  ctx.arc(500, 150, 10, 0, 2*Math.PI);
+  ctx.arc(500, 125, 10, 0, 2*Math.PI);
   ctx.stroke();
   ctx.fillStyle = 'blue';
   ctx.fill();
+//
+// vm.params.xNow = 500+vm.params.b*iteration*Math.cos(vm.params.a*Math.PI/180);
+// vm.params.yNow = 150+vm.params.b*iteration*Math.sin(vm.params.a*Math.PI/180);
 
   vm.shootBall = function(a, b) {
     console.log(a, b, vm.params.a, vm.params.b);
     setInterval(ball, 50);
+    setInterval(whereAreWe, 5);
+    //
+    // var xNow = (vm.params.xNow-500)/100;
+    // var yNow = (vm.params.yNow-100)/100;
+    //
+    // if ((Math.pow(xNow), 2) > yNow-10 && (Math.pow(xNow), 2) < yNow+10) {
+    //   console.log('meow!!!');
+    // }
+
   };
 
   vm.reset = function() {
     iteration = 0;
+    count = 0;
     ctx.clearRect(0,0,1000,1000);
     parabola(70, 5, 1, 0, 1, 1);
     ctx.transform(-1, 0, 0, 1, 0, 0);
     parabola(70, 5, 1, 0, 1, -1);
     ctx.transform(-1, 0, 0, 1, 0, 0);
     ctx.beginPath();
-    ctx.arc(500, 150, 10, 0, 2*Math.PI);
+    ctx.arc(500, 125, 10, 0, 2*Math.PI);
     ctx.stroke();
     ctx.fillStyle = 'blue';
     ctx.fill();
+    setInterval(whereAreWe, 20);
+
   };
 
-  var iteration = 0;
 
     function ball() {
 
@@ -45,7 +61,7 @@ app.controller('ParabShooterController', function () {
       parabola(70, 5, 1, 0, 1, -1);
       ctx.transform(-1, 0, 0, 1, 0, 0);
       ctx.beginPath();
-      ctx.arc(500, 150, 10, 0, 2*Math.PI);
+      ctx.arc(500, 125, 10, 0, 2*Math.PI);
       ctx.stroke();
       ctx.fillStyle = 'blue';
       ctx.fill();
@@ -54,7 +70,7 @@ app.controller('ParabShooterController', function () {
       var theta = vm.params.a*Math.PI/180;
 
         ctx.beginPath();
-        ctx.arc(500 + v*iteration*Math.cos(theta), 150 + v*iteration*Math.sin(theta), 10, 0, 2*Math.PI);
+        ctx.arc(500 + v*iteration*Math.cos(theta), 125 + v*iteration*Math.sin(theta), 10, 0, 2*Math.PI);
         // ok good, as anticipated,this is a (slow) linear speed:
         // ctx.arc(500, iteration*2, 10, 0, 2*Math.PI);
         ctx.stroke();
@@ -62,6 +78,25 @@ app.controller('ParabShooterController', function () {
         ctx.fill();
         iteration++;
 
+
+    } //end BALL
+
+var count = 0;
+    function whereAreWe() {
+      // if (count<50) {
+        var x = 500+vm.params.b*iteration*Math.cos(vm.params.a*Math.PI/180);
+        var y = 125+vm.params.b*iteration*Math.sin(vm.params.a*Math.PI/180);
+        // console.log('x: ', x, ',  y: ', y);
+
+            var xNow = (x-500)/100;
+            var yNow = (y-100)/100;
+            // console.log(xNow, yNow);
+
+            if (xNow*xNow < yNow+0.05 && xNow*xNow > yNow-0.05) {
+              console.log('meow!!!');
+            }
+      // }
+      count++;
     }
 
   function parabola(x, l, a, b, c, dir) {
